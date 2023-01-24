28cdca63d79e0acdbc406a2974185ab1f04be70a
Jason-Brody16 <jason.hilan.com@gmail.com>
2023-01-24 21:26:52 +0700

    24 Января

---
+++ lesson_18/script.js
@@ -0,0 +1,26 @@
+// let message;  // Объявили переменную
+// message =  'НА ТЕБЯ ОФОРМИЛИ КРЕДИТ ЛОХ КАПУСТНЫЙ'// присвоили этой переменной значение
+// //alert(message) //Вывели ее
+// console.log (message)
+// console.log (`НА ТЕБЯ ОФОРМИЛИ КРЕДИТ ЛОХ КАПУСТНЫЙ ${message}`)
+// //let color = prompt ("Сколько?");//
+// //alert (`Пользователь ответил - ${color}`);//
+
+
+// let h = 170
+// let m = 70
+
+// let = alert(m / (h/100) ** 2)
+
+// let a = 150
+// let b = 50
+
+// let = alert(b / (a/100) ** 2)
+
+// alert (m / (h/100) ** 2 > b / (a/100) ** 2)
+
+const name = "Даун";
+const job = "Даун";
+const firstJobYear = 30000;
+const today = 1941;
+console.log(name)
