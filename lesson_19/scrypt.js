e6ef0eac13d8bc4fa0538cdb5f5a91019b085540
movavi_school <abcsmir.ant@yandex.ru>
2023-01-31 21:27:33 +0700

    31 Января

---
+++ lesson_19/scrypt.js
@@ -0,0 +1,47 @@
+// console.log(" Первая строка  \n\ вторая строка \n\ третья строка")
+// years = 12;
+// if(years >=18){
+//     alert("krasava");
+// }else{
+//     alert("daun");
+//     alert("тебе не хватает 6 годов");
+// }
+// deneg = 50;
+
+// switch(deneg) {
+//     case 50:
+//         console.log("luck");
+//         break;
+//    case 25:
+//     console.log("unluck");
+//     break;
+//     default:
+//     console.log("Чо?");
+//     break;
+// } 
+
+// ДНИ НЕДЕЛИ
+
+// var days = [
+//     'Воскресенье',
+//     'Понедельник',
+//     'Вторник',
+//     'Среда',
+//     'Четверг',
+//     'Пятница',
+//     'Суббота'
+//   ];
+//   var d = new Date();
+//   var n = d.getDay();
+//   console.log(days[n]);
+//   alert(days[n])
+
+"use strict";
+
+  let hasFinishedEducation = false; 
+const passFinalTest = true;
+ //Если условие верно то изменить переменную hasFinishedEducation на true
+if(passFinalTest) hasFinishedEducation=true; //ошибка в переменной hasFinishedEducation
+ 
+//Если условие верно то вывести текст
+if(hasFinishedEducation) console.log("Поздравляем! вы");
