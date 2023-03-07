47de879e2fa96f1e4e507422875d2bf4e36b5706
movavi_school <nastya290909.icloud.com>
2023-03-07 21:26:05 +0700

    7 Марта

---
+++ lesson_24/script.js
@@ -0,0 +1,14 @@
+// let num = 3.1415
+// console.log(num.toFixed(20))
+// console.log(num.toFixed(3))
+
+// let num2 = 6.35
+// console.log(num2.toFixed(6))
+
+// let inp = document.
+// querySelector(".chiki-pike")
+// inp.value ="чикипике"
+
+// function myFunction() {
+//     document.querySelector(".chiki-pike").value = "Хаха ты лох"
+// }
